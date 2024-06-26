13337ca7c8c1805f6df021f6d6cde2a812cee416
satyam533patel <144761741+satyam533patel@users.noreply.github.com>
2024-06-26 23:43:59 +0530

    signup_page

---
+++ app/src/main/java/com/example/cashledger/SignupActivity.kt
@@ -0,0 +1,30 @@
+package com.example.cashledger
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_signup)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar_signup_activity)
+        setSupportActionBar(toolbar)
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.baseline_arrow_back_ios_24)
+        }
+        toolbar.setNavigationOnClickListener{onBackPressed()}
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+}
