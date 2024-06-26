79682927f4507d29ab92d510477764a5f619fa61
satyam533patel <144761741+satyam533patel@users.noreply.github.com>
2024-06-27 00:22:34 +0530

    signin_page

--- app/src/main/java/com/example/cashledger/IntroActivity.kt
+++ app/src/main/java/com/example/cashledger/IntroActivity.kt
@@ -12,9 +12,15 @@ class IntroActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_intro)
+        //sign-up
         val buttonSignup_intro = findViewById<Button>(R.id.button_signup_intro)
         buttonSignup_intro.setOnClickListener {
             startActivity(Intent(this,SignupActivity::class.java))
+        }
+        //sign-in
+        val buttonSignin_intro = findViewById<Button>(R.id.button_login_intro)
+        buttonSignin_intro.setOnClickListener {
+            startActivity(Intent(this,Signin_activity::class.java))
         }
     }
 }
